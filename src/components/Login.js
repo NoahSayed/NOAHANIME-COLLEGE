31f3d5cc85e13b8632ff558f4529af859bff70c4
Noah sayed <noahsayeddb@gmail.com>
2024-02-22 13:09:09 +0530

    Login Page Added Dont want to sign up button plus tokyo ghoul

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,42 @@
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
+
 
 const Login = (props) => {
   return (
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
-          <SignUp>GET ALL THERE</SignUp>
+        <Text> NoaHAnimE</Text>
+
+          <SignUp href="/Home.js  " >Dont Want to Sign Up</SignUp>
+          
+         
           <Description>
-            Get Premier Access to Raya and the Last Dragon for an additional fee
-            with a Disney+ subscription. As of 03/26/21, the price of Disney+
-            and The Disney Bundle will increase by $1.
+           Get a list of Top anime here including Noah's favourites. Highly recommended top
+           watch and if you dont like it then you should probably stop watching . Anime is not 
+           for You
           </Description>
-          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+
+         
         </CTA>
         <BgImage />
       </Content>
     </Container>
   );
 };
+const Text = styled.text`
+color :  #fff;
+font-family : Avenir-Roman, sans-serif;
+font-size : 100px;
+font-weight : 590;
+text-decoration-line: underline;
+text-shadow: 1px 1px 2px black , 0 0 2em black, 0 0 0.2em black , 0 0 0.2em black , 0 0 0.2em black;
+
+@media (max-width : 768px){
+  font-size:55px;
+}
+`;
 
 const Container = styled.section`
   overflow: hidden;
@@ -47,12 +65,17 @@ const BgImage = styled.div`
   background-position: top;
   background-size: cover;
   background-repeat: no-repeat;
-  background-image: url("/images/login-background.jpg");
+  background-image: url("https://wallpaperaccess.com/full/435.jpg");
   position: absolute;
   top: 0;
   right: 0;
   left: 0;
   z-index: -1;
+
+  @media (max-width : 768px){
+    position :absolute;
+    left : -280px;
+  }
 `;
 
 const CTA = styled.div`
@@ -93,12 +116,14 @@ const Description = styled.p`
   margin: 0 0 24px;
   line-height: 1.5;
   letter-spacing: 1.5px;
+  text-shadow: 1px 1px 2px black , 0 0 2em black, 0 0 0.2em black , 0 0 0.2em black , 0 0 0.2em black;
+
 `;
 
 const CTALogoTwo = styled.img`
   max-width: 600px;
   margin-bottom: 20px;
-  display: inline-block;
+  display: inli ne-block;
   vertical-align: bottom;
   width: 100%;
 `;
